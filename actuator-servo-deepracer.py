4ecb365781c3e81bd2fcd7c541731cf25d8d0281
HairyTurtle00 <hairyturtle00@gmail.com>
2025-07-09 09:58:40 -0500

    Cleaned up files for teleop and added RPI GPIO support

--- actuator-servo-deepracer.py
+++ actuator-servo-deepracer.py
@@ -1,91 +1,48 @@
-import os
+import pigpio
+import time
 import json
-import sys
-import params
 import atexit
 
-def write_to_file(path, val):
-    with open(path, 'w') as f:
-        f.write(val)
+PWM_CALIB_FILE = "pwm_calibration.json"
 
-class DeepracerServo():
-    GPIO_DIR = "/sys/class/gpio"
-    PWM_DIR = "/sys/class/pwm/pwmchip0"
-    PWM_PERIOD = "20000000"
-    PWM_CALIB_FILE = "pwm_calibration.json"
+# Use GPIO 18 and 19 for hardware PWM (or any if pigpiod is running)
+THROTTLE_PIN = 18  # GPIO18
+STEERING_PIN = 19  # GPIO19
 
+class PiServoController:
     def __init__(self, default_speed=0, calib_mode=False):
-        # read the calibration file
-        # The default values below will be overwritten
-        # with the values in calibration file
-        self.thr_lims = [1360500, 1468500, 1468500, 1536000]
-        self.srv_lims = [1230000, 1390000, 1390000, 1670000]
+        self.pi = pigpio.pi()
+        if not self.pi.connected:
+            raise Exception("Could not connect to pigpiod. Make sure it's running with 'sudo pigpiod'.")
+
+        # Defaults (us)
+        self.thr_lims = [1360, 1468, 1468, 1536]
+        self.srv_lims = [1230, 1390, 1390, 1670]
         self.thr_polarity = 1
         self.srv_polarity = 1
-        self.inputdev = __import__(params.inputdev)
 
         self.calib_mode = calib_mode
-
         self.read_calib_file()
-        print('Throttle pwm limits:', self.thr_lims)
-        print('Steering pwm limits:', self.srv_lims)
-        print('Polarities:', self.thr_polarity, self.srv_polarity)
-
-        if not os.path.exists(DeepracerServo.GPIO_DIR + "/gpio436"):
-            try:
-                write_to_file(DeepracerServo.GPIO_DIR + "/export", "436")
-            except OSError as e:
-                print(f"Failed to export gpio436")
-        gpio436_path = DeepracerServo.GPIO_DIR + "/gpio436"
-        try:
-            write_to_file(gpio436_path + "/direction", "out")
-            write_to_file(gpio436_path + "/value", "0")
-        except OSError as e:
-            print(f"Failed to configure gpio436")
-
-        # throttle and steering
-        for pwm in ("/pwm0", "/pwm1"):
-            if not os.path.exists(DeepracerServo.PWM_DIR + pwm):
-                try:
-                    write_to_file(DeepracerServo.PWM_DIR + "/export", pwm[-1])
-                except OSError as e:
-                    print(f"Failed to export {pwm}")
-
-            try:
-                write_to_file(DeepracerServo.PWM_DIR + pwm + "/period", DeepracerServo.PWM_PERIOD)
-                # Disable it first to set the polarity
-                # The direction of steering is determined by
-                # the pwm value, not the polarity.
-                write_to_file(DeepracerServo.PWM_DIR + pwm + "/enable", "0")
-                write_to_file(DeepracerServo.PWM_DIR + pwm + "/polarity", "normal")
-                write_to_file(DeepracerServo.PWM_DIR + pwm + "/enable", "1")
-                self.stop()
-            except OSError as e:
-                print(f"Failed to configure {pwm}")
         atexit.register(self.cleanup)
-
-    def cleanup(self):
-        self.save_calib_file()
 
     def read_calib_file(self):
         try:
-            with open(DeepracerServo.PWM_CALIB_FILE, 'r') as f:
+            with open(PWM_CALIB_FILE, 'r') as f:
                 calib = json.load(f)
             self.thr_lims = calib['throttle_limits']
             self.srv_lims = calib['steering_limits']
             self.thr_polarity = calib['throttle_polarity']
             self.srv_polarity = calib['steering_polarity']
-
         except:
             print('Could not read calibration file, using default values.')
 
     def save_calib_file(self):
-        with open(DeepracerServo.PWM_CALIB_FILE, 'w') as f:
+        with open(PWM_CALIB_FILE, 'w') as f:
             calib = {
-                'throttle_limits' : self.thr_lims,
-                'steering_limits' :  self.srv_lims,
-                'throttle_polarity' : self.thr_polarity,
-                'steering_polarity' : self.srv_polarity,
+                'throttle_limits': self.thr_lims,
+                'steering_limits': self.srv_lims,
+                'throttle_polarity': self.thr_polarity,
+                'steering_polarity': self.srv_polarity,
             }
             json.dump(calib, f)
 
@@ -93,36 +50,19 @@ class DeepracerServo():
         limits = self.thr_lims
         if self.thr_polarity == 1:
             if pct > 0:
-                limdiff = limits[3]-limits[2]
-                pwm = limits[2] + (pct * limdiff / 100)
+                pwm = limits[2] + pct * (limits[3] - limits[2]) / 100
             elif pct < 0:
-                limdiff = limits[1]-limits[0]
-                pwm = limits[1] + (pct * limdiff / 100)
+                pwm = limits[1] + pct * (limits[1] - limits[0]) / 100
             else:
-                pwm = limits[0] + (limits[3]-limits[0]) / 2
+                pwm = (limits[0] + limits[3]) / 2
         else:
             if pct > 0:
-                limdiff = limits[0]-limits[1]
-                pwm = limits[1] + (pct * limdiff / 100)
+                pwm = limits[1] + pct * (limits[0] - limits[1]) / 100
             elif pct < 0:
-                limdiff = limits[2]-limits[3]
-                pwm = limits[2] + (pct * limdiff / 100)
+                pwm = limits[2] + pct * (limits[2] - limits[3]) / 100
             else:
-                pwm = limits[0] + (limits[3]-limits[0]) / 2
-
+                pwm = (limits[0] + limits[3]) / 2
         return int(pwm)
-
-    # throttle_pct:v [-100, +100]
-    def set_throttle(self, throttle_pct):
-        # Ensure throttle_pwm is within the acceptable range
-        p = max(-100, min(100, throttle_pct))
-        pwm = self.get_pwm_thr(p)
-        #print(f"throttle_pct: {throttle_pct} throttle_pwm: {pwm}")
-
-        try:
-            write_to_file(DeepracerServo.PWM_DIR + '/pwm0/duty_cycle', str(pwm))
-        except OSError as e:
-            print(f"Failed to set throttle: {e}")
 
     def get_pwm_srv(self, pct):
         limits = self.srv_lims
@@ -131,169 +71,27 @@ class DeepracerServo():
 
         if pct == 0:
             pwm = (limits[1] + limits[2]) / 2
-        elif pct>0:
+        elif pct > 0:
             pwm = limits[2] + (limits[3] - limits[2]) * pct / 100
-        elif pct<0:
-            pwm = limits[1]+ (limits[1] - limits[0]) * pct / 100
+        elif pct < 0:
+            pwm = limits[1] + (limits[1] - limits[0]) * pct / 100
         return int(pwm)
 
-    # steering_deg: [-30, +30]
+    def set_throttle(self, throttle_pct):
+        pct = max(-100, min(100, throttle_pct))
+        pwm = self.get_pwm_thr(pct)
+        self.pi.set_servo_pulsewidth(THROTTLE_PIN, pwm)
+
     def set_steering(self, steering_deg):
-        # Ensure input is within the acceptable range
-        p = max(-30, min(30, steering_deg))
-        p = p / 30 * 100
-        pwm = self.get_pwm_srv(p)
-        #print(f"steering_deg: {steering_deg} steering_pwm: {pwm}")
-
-        try:
-            write_to_file(DeepracerServo.PWM_DIR + '/pwm1/duty_cycle', str(pwm))
-        except OSError as e:
-            print(f"Failed to set steering: {e}")
-
-    def ffw(self, throttle):
-        self.set_throttle(throttle)
+        deg = max(-30, min(30, steering_deg))
+        pct = deg / 30 * 100
+        pwm = self.get_pwm_srv(pct)
+        self.pi.set_servo_pulsewidth(STEERING_PIN, pwm)
 
     def stop(self):
-        self.set_throttle(0)
+        self.pi.set_servo_pulsewidth(THROTTLE_PIN, 0)
+        self.pi.set_servo_pulsewidth(STEERING_PIN, 0)
 
-    def drive_calib(self, init_pwm, plrty=1):
-        pwm = init_pwm
-        while True:
-            ch = self.inputdev.read_single_event()
-            if ch == ord('a'):
-                pwm -= 4000 * plrty
-            elif ch == ord('z'):
-                pwm += 4000 * plrty
-            elif ch == ord('s'): # switch polarity
-                plrty *= -1
-            elif ch == ord('q'):
-                break
-
-            try:
-                write_to_file(DeepracerServo.PWM_DIR + '/pwm0/duty_cycle', str(pwm))
-            except OSError as e:
-                print(f"Failed to set throttle: {e}")
-
-        return pwm, plrty
-
-    def steer_calib(self, init_pwm, plrty=1):
-        pwm = init_pwm
-        while True:
-            ch = self.inputdev.read_single_event()
-            if ch == ord('a'): # increase
-                pwm += 10000 * plrty
-            elif ch == ord('z'): # decrease
-                pwm -= 10000 * plrty
-            elif ch == ord('s'): # switch polarity
-                plrty *= -1
-            elif ch == ord('q'):
-                break
-            try:
-                write_to_file(DeepracerServo.PWM_DIR + '/pwm1/duty_cycle', str(pwm))
-            except OSError as e:
-                print(f"Failed to set steering: {e}")
-        return pwm, plrty
-
-    def do_drive_calib(self):
-        print('*****************************************')
-        print('Calibrating max forward throttle now.')
-        print('Use A to increase the forward throttle.')
-        print('Use Z to increase the backward throttle.')
-        print('If the opposite is happening, press S to switch polarity.')
-        print('Now adjust the throttle until desired maximum forward is reached.')
-        print('Use Q button when done.')
-        pwm1, p = self.drive_calib(self.thr_lims[1])
-        print('\n\n*****************************************')
-        print('Calibrating min forward throttle now.')
-        print('Use A to increase the forward throttle.')
-        print('Use Z to increase the backward throttle.')
-        print('Now decrease the throttle until it stops spinning.')
-        print('Then slowly increase the throttle until it starts spinning forward')
-        print('Use Q button when done.')
-        pwm2, p = self.drive_calib(pwm1, p)
-        print('\n\n*****************************************')
-        print('Calibrating max backward throttle now.')
-        print('Use A to increase the forward throttle.')
-        print('Use Z to increase the backward throttle.')
-        print('Now adjust the throttle until desired maximum backward is reached.')
-        print('Use Q button when done.')
-        pwm3, p = self.drive_calib(pwm2, p)
-        print('\n\n*****************************************')
-        print('Calibrating min backward throttle now.')
-        print('Use A to increase the forward throttle.')
-        print('Use Z to increase the backward throttle.')
-        print('Now decrease the throttle until it stops spinning.')
-        print('Then slowly increase the throttle until it starts spinning backward')
-        print('Use Q button when done.')
-        pwm4, p = self.drive_calib(pwm3, p)
-
-        self.thr_polarity = -1 if pwm1 < pwm3 else 1
-        if self.thr_polarity == -1:
-            self.thr_lims = [pwm1, pwm2, pwm4, pwm3]
-        else:
-            self.thr_lims = [pwm3, pwm4, pwm2, pwm1]
-
-        self.save_calib_file()
+    def cleanup(self):
         self.stop()
-
-    def do_steering_calib(self):
-        #Begin steering calibration
-        print('\n\n*****************************************')
-        print('Calibrating max left steering angle now.')
-        print('Use A to increase the left turn angle.')
-        print('Use Z to increase the right turn angle.')
-        print('If the opposite is happening, press S to switch polarity.')
-        print('Adjust the turning angle until the maximum left point is reached,')
-        print('where increasing the angle doesnt change anything.')
-        print('Use Q button when done.')
-        spwm1, p = self.steer_calib(self.srv_lims[1])
-        print('\n\n*****************************************')
-        print('Calibrating min left steering angle now.')
-        print('Use A to increase the left turn angle.')
-        print('Use Z to increase the right turn angle.')
-        print('Decrease the left turning angle until the tires on the right side are aligned.')
-        print('Use Q button when done.')
-        spwm2, p = self.steer_calib(spwm1, p)
-        print('\n\n*****************************************')
-        print('Calibrating max right steering angle now.')
-        print('Use A to increase the left turn angle.')
-        print('Use Z to increase the right turn angle.')
-        print('Adjust the turning angle until the maximum right point is reached,')
-        print('where increasing the angle doesnt change anything.')
-        print('Use Q button when done.')
-        spwm3, p = self.steer_calib(spwm2, p)
-        print('\n\n*****************************************')
-        print('Calibrating min right steering angle now.')
-        print('Use A to increase the left turn angle.')
-        print('Use Z to increase the right turn angle.')
-        print('Decrease the left turning angle until the tires on the left side are aligned.')
-        print('Use Q button when done.')
-        spwm4, p = self.steer_calib(spwm3, p)
-
-        self.srv_polarity = -1 if spwm1 < spwm3 else 1
-        if self.srv_polarity == -1:
-            self.srv_lims = [spwm1, spwm2, spwm4, spwm3]
-        else:
-            self.srv_lims = [spwm3, spwm4, spwm2, spwm1]
-
-        self.save_calib_file()
-
-    def shift_lims_left(self):
-        self.thr_lims = [l  - 8000 for l in self.thr_lims]
-
-    def shift_lims_right(self):
-        self.thr_lims = [l  + 8000 for l in self.thr_lims]
-
-if __name__ == "__main__":
-    drs = DeepracerServo(0, True)
-
-    calib = {}
-    if sys.argv[1] == "calib_throttle":
-        drs.do_drive_calib()
-        print('\nDone!\n')
-    elif sys.argv[1] == "calib_steering":
-        drs.do_steering_calib()
-        print('\nDone!\n')
-    else:
-        print('Usage 1: python3 actuator-servo-deepracer.py calib_throttle')
-        print('Usage 2: python3 actuator-servo-deepracer.py calib_steering')
+        self.pi.stop()
--- deeppicar.py
+++ deeppicar.py
@@ -1,37 +1,15 @@
 #!/usr/bin/python
-import os
-import time
-import atexit
-import cv2
 import math
 import numpy as np
-import sys
 import params
 import argparse
 import input_stream
 
-from PIL import Image, ImageDraw
-
 ##########################################################
 # import deeppicar's sensor/actuator modules
 ##########################################################
-camera   = __import__(params.camera)
 actuator_servo = __import__(params.actuator)
-
-##########################################################
-# global variable initialization
-##########################################################
-use_thread = True
-view_video = False
-fpv_video = False
-enable_record = False
-cfg_cam_res = (160, 120)
-cfg_cam_fps = 15
-
-frame_id = 0
-max_frames = 60 * cfg_cam_fps  # 950
 angle = 0.0
-period = 1.0 / cfg_cam_fps # sec (=66ms)
 
 ##########################################################
 # local functions
@@ -41,108 +19,20 @@ def deg2rad(deg):
 def rad2deg(rad):
     return 180.0 * rad / math.pi
 
-def g_tick():
-    t = time.time()
-    count = 0
-    while True:
-        count += 1
-        yield max(t + count*period - time.time(),0)
-
-def turn_off():
-    actuator.stop()
-    camera.stop()
-    if frame_id > 0:
-        keyfile.close()
-        vidfile.release()
-
-def crop_image(img):
-    scaled_img = cv2.resize(img, (max(int(params.img_height * 4 / 3), params.img_width), params.img_height))
-    fb_h, fb_w, fb_c = scaled_img.shape
-    # print(scaled_img.shape)
-    startx = int((fb_w - params.img_width) / 2);
-    starty = int((fb_h - params.img_height) / 2);
-    return scaled_img[starty:starty+params.img_height, startx:startx+params.img_width,:]
-
-def preprocess(img):
-    if args.pre == "crop":
-        img = crop_image(img)
-    else:
-        img = img[img.shape[0]//6:]
-        img = cv2.resize(img, (params.img_width, params.img_height))
-    # Convert to grayscale and readd channel dimension
-    if params.img_channels == 1:
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        img = np.reshape(img, (params.img_height, params.img_width, params.img_channels))
-    img = img / 255.
-    return img
-
-def overlay_image(l_img, s_img, x_offset, y_offset):
-    assert y_offset + s_img.shape[0] <= l_img.shape[0]
-    assert x_offset + s_img.shape[1] <= l_img.shape[1]
-
-    l_img = l_img.copy()
-    for c in range(0, 3):
-        l_img[y_offset:y_offset+s_img.shape[0],
-              x_offset:x_offset+s_img.shape[1], c] = (
-                  s_img[:,:,c] * (s_img[:,:,3]/255.0) +
-                  l_img[y_offset:y_offset+s_img.shape[0],
-                        x_offset:x_offset+s_img.shape[1], c] *
-                  (1.0 - s_img[:,:,3]/255.0))
-    return l_img
-
-def print_stats(execution_times):
-    # Calculate statistics
-    avg = np.mean(execution_times)
-    min = np.min(execution_times)
-    max = np.max(execution_times)
-    p99 = np.percentile(execution_times, 99)
-    p90 = np.percentile(execution_times, 90)
-    p50 = np.percentile(execution_times, 50)
-
-    print(f"Average Execution Time: {avg:.6f} seconds")
-    print(f"Minimum Execution Time: {min:.6f} seconds")
-    print(f"Maximum Execution Time: {max:.6f} seconds")
-    print(f"99th Percentile Execution Time: {p99:.6f} seconds")
-    print(f"90th Percentile Execution Time: {p90:.6f} seconds")
-    print(f"50th Percentile Execution Time: {p50:.6f} seconds")
-
-def measure_execution_time(func, num_trials):
-    execution_times = []
-    for _ in range(num_trials):
-        start_time = time.time()
-        func()  # Call the function to measure its execution time
-        end_time = time.time()
-        execution_time = end_time - start_time
-        execution_times.append(execution_time)
-    # Calculate statistics
-    print_stats(execution_times)
-
 ##########################################################
 # program begins
 ##########################################################
 
 parser = argparse.ArgumentParser(description='DeepPicar main')
-parser.add_argument("-d", "--dnn", help="Enable DNN", action="store_true")
 parser.add_argument("-t", "--throttle", help="throttle percent. [0-100]%", type=int, default=50)
 parser.add_argument("-n", "--ncpu", help="number of cores to use.", type=int, default=2)
 parser.add_argument("-f", "--hz", help="control frequnecy", type=int)
-parser.add_argument("--fpvvideo", help="Take FPV video of DNN driving", action="store_true")
-parser.add_argument("--use", help="use [tflite|tf|openvino]", type=str, default="openvino")
-parser.add_argument("--pre", help="preprocessing [resize|crop]", type=str, default="resize")
 parser.add_argument("-g", "--gamepad", help="Use gamepad", action="store_true")
 args = parser.parse_args()
 
 if args.throttle:
     print ("throttle = %d pct" % (args.throttle))
-if args.hz:
-    period = 1.0/args.hz
-    print("new period: ", period)
-if args.fpvvideo:
-    fpv_video = True
-    print("FPV video of DNN driving is on")
 
-print("period (sec):", period)
-print("preprocessing:", args.pre)
 
 if args.gamepad:
     cur_inp_type= input_stream.input_type.GAMEPAD
@@ -151,73 +41,16 @@ else:
 
 cur_inp_stream= input_stream.instantiate_inp_stream(cur_inp_type, args.throttle)
 
-if not os.path.exists("dataset"):
-    # Create the directory
-    os.makedirs("dataset")
 
-##########################################################
-# import deeppicar's DNN model
-##########################################################
-print ("Loading model: " + params.model_file)
-
-print("use:", args.use)
-if args.use == "tf":
-    from tensorflow import keras
-    model = keras.models.load_model(params.model_file+'.h5')
-elif args.use == "openvino":
-    import openvino as ov
-    core = ov.Core()
-    ov_model = core.read_model(params.model_file+'.tflite')
-    model = core.compile_model(ov_model, 'AUTO')
-else:
-    try:
-        # Import TFLite interpreter from tflite_runtime package if it's available.
-        from tflite_runtime.interpreter import Interpreter
-        interpreter = Interpreter(params.model_file+'.tflite', num_threads=args.ncpu)
-    except ImportError:
-        # Import TFLMicro interpreter
-        try:
-            from tflite_micro_runtime.interpreter import Interpreter 
-            interpreter = Interpreter(params.model_file+'.tflite')
-        except:
-            # If all failed, fallback to use the TFLite interpreter from the full TF package.
-            import tensorflow as tf
-            interpreter = tf.lite.Interpreter(model_path=params.model_file+'.tflite', num_threads=args.ncpu)
-
-    interpreter.allocate_tensors()
-    input_index = interpreter.get_input_details()[0]["index"]
-    output_index = interpreter.get_output_details()[0]["index"]
 
 
 # initlaize deeppicar modules
 actuator = actuator_servo.DeepracerServo(args.throttle)
-camera.init(res=cfg_cam_res, fps=cfg_cam_fps, threading=use_thread)
-atexit.register(turn_off)
-
-g = g_tick()
-start_ts = time.time()
-
-frame_arr = []
-angle_arr = []
-actuator_times = []
 
 # enter main loop
 while True:
-    if use_thread:
-        time.sleep(next(g))
-    frame = camera.read_frame()
-    if frame is None:
-        print("frame is None")
-        break
-    ts = time.time()
 
-    # receive input (must be non blocking)
-    #ch = inputdev.read_single_event()
-    if view_video:
-        cv2.imshow('frame', frame)
-        ch = cv2.waitKey(1) & 0xFF
-    else:
-        command, direction, speed = cur_inp_stream.read_inp()
+    command, direction, speed = cur_inp_stream.read_inp()
 
     if command == 'a':
         start_ts = ts
@@ -228,15 +61,6 @@ while True:
         args.dnn = False # manual mode
     elif command == 'z':
         print (f"reverse, speed: {speed}%")
-    elif command == 'r':
-        enable_record = not enable_record
-        print ("record mode: ", enable_record)
-    elif command == 't':
-        print ("toggle video mode is NOT SUPPORTED")
-        #view_video = not view_video
-    elif command == 'd':
-        print ("toggle DNN mode")
-        args.dnn = not args.dnn
     elif command == 'y':
         actuator.shift_lims_left()
     elif command == 'h':
@@ -245,76 +69,8 @@ while True:
         actuator.stop()
         break
 
-    if args.dnn == True:
-        # 1. machine input
-        img = preprocess(frame)
-        img = np.expand_dims(img, axis=0).astype(np.float32)
-        if args.use == "tf":
-            angle = model.predict(img)[0]
-        elif args.use == "openvino":
-            angle = model(img)[0][0][0]
-        else: # tflite
-            interpreter.set_tensor(input_index, img)
-            interpreter.invoke()
-            angle = interpreter.get_tensor(output_index)[0][0]
-
-        steering_deg = rad2deg(angle)
-        actuator.set_steering(steering_deg)
-    else:
-        actuator.set_steering(direction * 30)
-        angle = deg2rad(direction * 30)
+    actuator.set_steering(direction * 30)
+    angle = deg2rad(direction * 30)
     actuator.set_throttle(speed)
 
-    dur = time.time() - ts
-    if dur > period:
-        print("%.3f: took %d ms - deadline miss."
-                % (ts - start_ts, int(dur * 1000)))
-    # else:
-    #     print("%.3f: took %d ms" % (ts - start_ts, int(dur * 1000)))
-
-    if enable_record == True and frame_id == 0:
-        # create files for data recording
-        keyfile = open(params.rec_csv_file, 'w+')
-        keyfile.write("ts,frame,wheel\n") # ts (ms)
-        try:
-            fourcc = cv2.cv.CV_FOURCC(*'XVID')
-        except AttributeError as e:
-            fourcc = cv2.VideoWriter_fourcc(*'XVID')
-        vidfile = cv2.VideoWriter(params.rec_vid_file, fourcc,
-                                cfg_cam_fps, cfg_cam_res)
-    if enable_record == True and frame is not None:
-        # increase frame_id
-        frame_id += 1
-
-        # write input (angle)
-        str = "{},{},{}\n".format(int(ts*1000), frame_id, angle)
-        keyfile.write(str)
-
-        if args.dnn and fpv_video:
-            textColor = (255,255,255)
-            bgColor = (0,0,0)
-            newImage = Image.new('RGBA', (100, 20), bgColor)
-            drawer = ImageDraw.Draw(newImage)
-            drawer.text((0, 0), "Frame #{}".format(frame_id), fill=textColor)
-            drawer.text((0, 10), "Angle:{}".format(angle), fill=textColor)
-            newImage = cv2.cvtColor(np.array(newImage), cv2.COLOR_BGR2RGBA)
-            frame = overlay_image(frame,
-                                     newImage,
-                                     x_offset = 0, y_offset = 0)
-        # write video stream
-        vidfile.write(frame)
-        #img_name = "cal_images/opencv_frame_{}.png".format(frame_id)
-        #cv2.imwrite(img_name, frame)
-        if frame_id >= max_frames:
-            print (f"recorded {max_frames} frames")
-            break
-        print ("%.3f %d %.3f %d(ms)" %
-           (ts, frame_id, angle, int((time.time() - ts)*1000)))
-    # update previous steering angle
-
-
 print ("Finish..")
-if len(actuator_times):
-    print ("Actuator latency measurements: {} trials".format(len(actuator_times)))
-    print_stats(actuator_times)
-turn_off()
