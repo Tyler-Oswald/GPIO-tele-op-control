c45118217a7c8d9ad414edd48363799a1117a87d
HairyTurtle00 <hairyturtle00@gmail.com>
2025-07-09 13:31:16 -0500

    Fine tuning

--- pi_gpio_controller.py
+++ pi_gpio_controller.py
@@ -16,7 +16,7 @@ class PiServoController:
         # Throttle range (for ESC)
         self.THROTTLE_MIN = 1060     # reverse
         self.THROTTLE_STOP = 1060    # neutral
-        self.THROTTLE_MAX = 1600    # forward 
+        self.THROTTLE_MAX = 1700    # forward 
 
 
 
