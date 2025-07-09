d152dddc659a29d35358026ccc28469022347d68
HairyTurtle00 <hairyturtle00@gmail.com>
2025-07-09 13:35:51 -0500

    Fine tuning

--- deeppicar.py
+++ deeppicar.py
@@ -12,7 +12,7 @@ STEERING_RIGHT = 1670
 
 # ESC PWM values
 THROTTLE_MIN = 1060     # reverse
-THROTTLE_STOP = 1060   # neutral
+THROTTLE_STOP = 1500   # neutral
 THROTTLE_MAX = 1700    # forward Normal 1860
 
 def scale(val, in_min, in_max, out_min, out_max):
--- pi_gpio_controller.py
+++ pi_gpio_controller.py
@@ -15,7 +15,7 @@ class PiServoController:
 
         # Throttle range (for ESC)
         self.THROTTLE_MIN = 1060     # reverse
-        self.THROTTLE_STOP = 1060    # neutral
+        self.THROTTLE_STOP = 1500    # neutral
         self.THROTTLE_MAX = 1700    # forward 
 
 
