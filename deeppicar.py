96918223b60c3e482acc29d117fa5b157a585e61
HairyTurtle00 <hairyturtle00@gmail.com>
2025-07-09 13:13:18 -0500

    Fine tuning

--- deeppicar.py
+++ deeppicar.py
@@ -13,7 +13,7 @@ STEERING_RIGHT = 1670
 # ESC PWM values
 THROTTLE_MIN = 1060     # reverse
 THROTTLE_STOP = 1060   # neutral
-THROTTLE_MAX = 1500    # forward Normal 1860
+THROTTLE_MAX = 1600    # forward Normal 1860
 
 def scale(val, in_min, in_max, out_min, out_max):
     return int((val - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
