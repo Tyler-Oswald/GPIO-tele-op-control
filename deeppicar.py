cfe0679b185f2aa6b850fdc184867569e9f6d0d8
HairyTurtle00 <hairyturtle00@gmail.com>
2025-07-09 14:04:28 -0500

    Update deeppicar.py

--- deeppicar.py
+++ deeppicar.py
@@ -30,8 +30,8 @@ try:
         command, direction, speed = inp.read_inp()
         if speed > USER_SPEED_LIMIT:
             speed = USER_SPEED_LIMIT
-        if speed < -USER_SPEED_LIMIT -10:
-            speed = -USER_SPEED_LIMIT - 10
+        if speed < -USER_SPEED_LIMIT -40:
+            speed = -USER_SPEED_LIMIT - 40
 
         # Cap speed
         print(f"Steering: {direction:.2f}  Speed: {speed:.1f}")
